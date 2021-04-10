b0e9c88846ceb527c7ceba33f72897b80833807a
jsmason <>
2021-04-10 23:50:29 +0000

    Initial commit

---
+++ TestCode.h
@@ -0,0 +1,16 @@
+#ifndef TEST_CODE_H
+#define TEST_CODE_H
+
+class TestCode
+{
+public: 
+
+	static void deleteDefaultPieces();
+  static void testPawns();
+  static void testRooks();
+  static void testKings();
+  static void testBishops();
+  static void testMakeMove();
+};
+
+#endif
